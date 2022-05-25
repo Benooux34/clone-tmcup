ce58e5815a2728a0a76336f843a12d60c4886a5a
draizKUN <benoit.antunes@ecoles-wis.net>
2022-05-26 00:10:28 +0200

    guts

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
 
       <div className="flex sm:justify-between">
         {/* TEXT */}
-        <div className="sm:w-[50%] ml-[50px]">
+        <div className="sm:w-[40%] ml-[50px]">
           <div className="text-center text-white font-bold leading-tight">
             <motion.h1 whileHover={{scale: 1.1}} className="sm:text-[80px] outline hover:text-white transition-all duration-500">TRACKMANIA CUP</motion.h1>
             <h2 className="sm:text-[24px] tracking-[10px]">2022 EDITION</h2>
