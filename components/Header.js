ebe81ec9f773f69e26523e3b517a9473383bb8c8
draizKUN <benoit.antunes@ecoles-wis.net>
2022-05-25 23:36:40 +0200

    sixteenp

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className="m-[50px] mb-10 flex justify-center sm:justify-start">
+    <div className="m-[50px] mb-[60px] flex justify-center sm:justify-start">
         <img className="h-[100px] sm:h-[120px]" src="logo.svg" alt="" />
     </div>
   )
